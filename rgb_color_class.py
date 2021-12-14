440e5c1a6b9b7e9a7c6d5839fe4c041fafda97f9
Vladimir Zimin <zima.vld@gmail.com>
2021-12-14 18:59:08 +0300

    Fix. Flake8 rules for color class.

--- rgb_color_class.py
+++ rgb_color_class.py
@@ -46,7 +46,8 @@ class Color(ComputerColor):
         self.blue = blue
 
     def __str__(self):
-        return f'{self.START};{self.red};{self.green};{self.blue}{self.MOD}●{self.END}{self.MOD}'
+        return f'{self.START};{self.red};{self.green};' \
+               f'{self.blue}{self.MOD}●{self.END}{self.MOD}'
 
     def __repr__(self):
         return str(self)
@@ -79,15 +80,20 @@ class Color(ComputerColor):
 
 def print_a(color: ComputerColor):
     bg_color = 0.2 * color
-    a_matrix = [
+    a_matrix = [  # 79 characters inline pep8
         [bg_color] * 19,
         [bg_color] * 9 + [color] + [bg_color] * 9,
-        [bg_color] * 8 + [color] * 3 + [bg_color] * 8,
-        [bg_color] * 7 + [color] * 2 + [bg_color] + [color] * 2 + [bg_color] * 7,
-        [bg_color] * 6 + [color] * 2 + [bg_color] * 3 + [color] * 2 + [bg_color] * 6,
+        [bg_color] * 8 + [color] * 3 +
+        [bg_color] * 8,
+        [bg_color] * 7 + [color] * 2 +
+        [bg_color] + [color] * 2 + [bg_color] * 7,
+        [bg_color] * 6 + [color] * 2 +
+        [bg_color] * 3 + [color] * 2 + [bg_color] * 6,
         [bg_color] * 5 + [color] * 9 + [bg_color] * 5,
-        [bg_color] * 4 + [color] * 2 + [bg_color] * 7 + [color] * 2 + [bg_color] * 4,
-        [bg_color] * 3 + [color] * 2 + [bg_color] * 9 + [color] * 2 + [bg_color] * 3,
+        [bg_color] * 4 + [color] * 2 +
+        [bg_color] * 7 + [color] * 2 + [bg_color] * 4,
+        [bg_color] * 3 + [color] * 2 +
+        [bg_color] * 9 + [color] * 2 + [bg_color] * 3,
         [bg_color] * 19,
     ]
     for row in a_matrix:
